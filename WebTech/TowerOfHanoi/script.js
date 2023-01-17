01763f96125381ab4fa813555d665e006da6ab65
shazzad5709 <shazzad1111@gmail.com>
2023-01-17 22:15:35 +0600

    classwork tower of hanoi done

--- WebTech/TowerOfHanoi/script.js
+++ WebTech/TowerOfHanoi/script.js
@@ -1,15 +1,33 @@
-var top1 = 0;
-var top2, top3 = 3;
+
+
+function move(from, to) {
+    let fromNode = document.getElementById(from)
+    let toNode = document.getElementById(to)
+    toNode.insertBefore(fromNode.firstElementChild, toNode.firstElementChild)
+}
 
 function move12() {
-    if(top1<4 || top2<4) {
-        let l = document.getElementById('first');
-    let node = document.getElementById('second');
-    
-    node.insertBefore(l.children[top1], node.children[top2]);
-    l.insertBefore(node.children[top2], l.children[top1]);
-    top1++;
-    top2--;
-    }
-    
+    move("first", "second");
 }
+
+function move13() {
+    move("first", "third");
+}
+
+function move21() {
+    move("second", "first");
+}
+
+function move23() {
+    move("second", "third");
+}
+
+function move31() {
+    move("third", "first");
+}
+
+function move32() {
+    move("third", "second");
+}
+
+
