0660376c3cb171a23a9d691480276ea774e51ef9
shazzad5709 <shazzad1111@gmail.com>
2023-01-23 13:03:56 +0600

    nxkljvn jklvxn x

--- DesignPattern/Creational/AbstractFactory/PMWidgetFactory.java
+++ DesignPattern/Creational/AbstractFactory/PMWidgetFactory.java
@@ -1,7 +1,10 @@
 public class PMWidgetFactory extends WidgetFactory{
+    @Override
     public Window createWindow() {
         return new PMWindow();
     }
+    
+    @Override
     public ScrollBar createScrollBar() {
         return new PMScrollBar();
     }
--- WebTech/TowerOfHanoi/script.js
+++ WebTech/TowerOfHanoi/script.js
@@ -53,3 +53,4 @@ function move32() {
 }
 
 
+
