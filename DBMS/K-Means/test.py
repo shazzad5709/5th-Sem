38fd2b91dfb9e77710da67de542a418b53bc11a3
shazzad5709 <shazzad1111@gmail.com>
2023-02-15 11:47:06 +0600

    ssbfsb

--- DBMS/K-Means/test.py
+++ DBMS/K-Means/test.py
@@ -3,15 +3,16 @@ from numpy import genfromtxt
 import numpy as np
 from KMeans import KMeans
 from sklearn.model_selection import train_test_split
+from sklearn import datasets
 
 if __name__ == "__main__":
     np.random.seed(42)
 
-    # data = np.genfromtxt('DBMS\Datasets\BreastCancer\Breast_cancer_data.csv', delimiter=',')
-    # X, y = np.array(list(zip(data[:, 3], data[:, 2]))) , data[:, -1]
-    X, y = make_blobs(
-        centers=3, n_samples=500, n_features=2, shuffle=True, random_state=40
-    )
+    data = np.genfromtxt('DBMS/Datasets/Iris/iris.csv', delimiter=',')
+    X, y = np.array(list(zip(data[:, 0], data[:, 1]))) , data[:, -1]
+    # X, y = make_blobs(
+    #     centers=3, n_samples=500, n_features=2, shuffle=True, random_state=40
+    # )
 
     clusters = len(np.unique(y))
     print(clusters)
