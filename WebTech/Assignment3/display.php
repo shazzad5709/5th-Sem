6b2a9ff4d42cd5cb731f37ef8ad9a35ab9184f25
shazzad5709 <shazzad1111@gmail.com>
2023-02-25 00:13:04 +0600

    vxvc

--- WebTech/Assignment3/display.php
+++
@@ -1,4 +0,0 @@
-<p>Name: <?php echo $_GET['name']; ?></p>
-<p>Email: <?php echo $_GET['email']; ?></p>
-<p>Address: <?php echo $_GET['address']; ?></p>
-<p>Photo: <img src="uploads/<?php echo $_GET['photo']; ?>"></p>
